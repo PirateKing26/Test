f192d7cc271b5d1f5b44c5acbbbc818015a7e4a6
Erby Magallanes <erbymagallanes@smart.com>
2016-02-09 15:03:19 +0800

    2nd Commit - add a single output change

--- testdemo2/src/testdemo2/Main.java
+++ testdemo2/src/testdemo2/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void displayGreeting() {
 		System.out.println("HELLO");
 		System.out.println("This app shows sales data");
+		System.out.println("Test 1");
 	}
 	
 	public static void main(String[] args){
